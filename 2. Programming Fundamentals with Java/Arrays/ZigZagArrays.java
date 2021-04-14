76531162f18929e8d0ab11eddb452ee497bfc5c7
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-04-14 18:48:33 +0200

    Update ZigZagArrays.java

--- 2. Programming Fundamentals with Java/Arrays/ZigZagArrays.java
+++ 2. Programming Fundamentals with Java/Arrays/ZigZagArrays.java
@@ -1,35 +1,29 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class ZigZagArrays {
+public class ArrayRotation {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] line = scanner.nextLine().split(" ");
+        int rotations = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[line.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(line[i]);
+        }
 
-        int[] first = new int[n];
-        int[] second = new int[n];
-
-        for (int i = 1; i <= n; i++) {
-            String[] line = scanner.nextLine().split(" ");
-
-            if (i % 2 == 0){
-                first[i - 1] = Integer.parseInt(line[1]);
-                second[i - 1] = Integer.parseInt(line[0]);
-            }else {
-                first[i - 1] = Integer.parseInt(line[0]);
-                second[i - 1] = Integer.parseInt(line[1]);
+        for (int i = 0; i < rotations; i++) {
+            int firstElement = numbers[0];
+            // shift elements to the left
+            for (int j = 0; j < numbers.length - 1; j++) {
+                numbers[j] = numbers[j + 1];
             }
+            // add first element to last position
+            numbers[numbers.length - 1] = firstElement;
         }
 
-        for (int j : first) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
-
-        for (int k : second){
-            System.out.print(k + " ");
+        for (int x: numbers){
+            System.out.print(x + " ");
         }
     }
 }
+
