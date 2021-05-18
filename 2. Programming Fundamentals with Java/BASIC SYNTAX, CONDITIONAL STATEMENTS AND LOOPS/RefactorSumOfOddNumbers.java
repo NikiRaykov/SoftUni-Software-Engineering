52d305e221cf2e0bcbf0fc35439089900b5a4f9a
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-05-18 07:56:30 +0200

    Add files via upload

--- 2. Programming Fundamentals with Java/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/RefactorSumOfOddNumbers.java
+++ 2. Programming Fundamentals with Java/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/RefactorSumOfOddNumbers.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
-public class RefactorSumOfOddNumbers {
-    public static void main(String[] args) {
+// You are assigned to find and fix the bugs in an existing piece of code, using the debugger. You should trace the program
+// execution to find the lines of code that produce incorrect or unexpected results.
+//You are given a program (existing source code) that prints the next n odd numbers (starting from 1) and
+// on the last row prints the sum of them.
 
-        Scanner scanner = new Scanner(System.in);
+public class RefactorSumOfOddNumbers {
+    public static void main(String[] args)    {
+            Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-        int sum = 1;
-        for (int i = 0; i <=n; i++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
             System.out.println(2 * i + 1);
-            sum += i;
+            sum += 2 * i + 1;
         }
         System.out.printf("Sum: %d%n", sum);
     }
 }
+
