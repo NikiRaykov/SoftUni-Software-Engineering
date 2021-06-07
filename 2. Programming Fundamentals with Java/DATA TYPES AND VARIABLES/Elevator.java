ea2d2015852a068a7ca6c4bded3526034a5d1b7c
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-06-07 21:41:59 +0200

    Add files via upload

--- 2. Programming Fundamentals with Java/DATA TYPES AND VARIABLES/Elevator.java
+++ 2. Programming Fundamentals with Java/DATA TYPES AND VARIABLES/Elevator.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
+//Calculate how many courses will be needed to elevate n persons by using an elevator with capacity of p persons.
+//The input holds two lines: the number of people n and the capacity p of the elevator.
+
 
 public class Elevator {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         int numberOfPeople = Integer.parseInt(scanner.nextLine());
-        int capacity = Integer.parseInt(scanner.nextLine());
+        int capacityOfTheElevator = Integer.parseInt(scanner.nextLine());
+        int courses;
 
-        int courses = (int)Math.ceil((double) numberOfPeople / capacity);
+        courses = (int) Math.ceil((double) numberOfPeople / capacityOfTheElevator);
         System.out.println(courses);
     }
 }
--- 2. Programming Fundamentals with Java/DATA TYPES AND VARIABLES/SumOfChars.java
+++ 2. Programming Fundamentals with Java/DATA TYPES AND VARIABLES/SumOfChars.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
+// Write a program, which sums the ASCII codes of n characters.
+//Print the sum on the console.
+
 
 public class SumOfChars {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        int sum = 0; int asciiValue = 0;
+        int numberOfLines = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
 
-        for (int i = 1; i <= n; i++) {
-            char letters = scanner.next().charAt(0);
-            asciiValue = (int)letters;
-            sum += asciiValue;
+        for (int i = 0; i < numberOfLines; i++) {
+            char ch = scanner.nextLine().charAt(0);
+            sum += (int) ch;
         }
-
         System.out.printf("The sum equals: %d", sum);
     }
 }
