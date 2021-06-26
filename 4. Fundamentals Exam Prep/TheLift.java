3dbe9f7822cc472b55464008f817fd981b40d508
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-06-26 14:57:47 +0200

    Add files via upload

---
+++ 4. Fundamentals Exam Prep/TheLift.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class TheLift {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int peopleWaiting = Integer.parseInt(scanner.nextLine());
+
+        if (peopleWaiting <= 0){
+            return;
+        }
+
+        int maxCapacity = 4;
+
+        List<Integer> lift = Arrays.stream(scanner.nextLine().split(" ")).
+                map(Integer::parseInt).collect(Collectors.toList());
+
+
+        for (int i = 0; i < lift.size(); i++) {
+
+            if (lift.get(i) < 0){
+                continue;
+            }
+
+
+            if (lift.get(i) > 0){
+                int oldValue = lift.get(i);
+                int peopleWhoGotInside = maxCapacity - oldValue;
+                lift.set(i, lift.get(i) + peopleWhoGotInside);
+                peopleWaiting -= peopleWhoGotInside;
+            }else {
+                lift.set(i, Math.min(peopleWaiting, maxCapacity));
+                peopleWaiting -= maxCapacity;
+            }
+
+        }
+
+        for (int i = 0; i < lift.size(); i++) {
+            if (lift.get(i) < 4){
+                System.out.println("The lift has empty spots!");
+            }
+        }
+
+        if (peopleWaiting > 0){
+            System.out.printf("There isn't enough space! %d people in a queue!%n", peopleWaiting);
+        }
+
+        for (Integer n: lift){
+            System.out.print(n + " ");
+        }
+    }
+}
