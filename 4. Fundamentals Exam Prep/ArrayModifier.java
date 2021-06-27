087bb4328d480c97601142d171558c7b1fb18040
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-06-27 11:25:54 +0200

    Update ArrayModifier.java

--- 4. Fundamentals Exam Prep/ArrayModifier.java
+++ 4. Fundamentals Exam Prep/ArrayModifier.java
@@ -1,47 +1,48 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-
+ 
 public class ArrayModifier {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-
-        String commands = scanner.nextLine();
-
-        while (!commands.equals("end")){
-            String[] tokens = commands.split(" ");
-
-            switch (tokens[0]){
+        Scanner scan = new Scanner(System.in);
+ 
+        int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+ 
+        String input = scan.nextLine();
+        while (!"end".equals(input)){
+            String[] commands = input.split("\\s+");
+            String token = commands[0];
+            switch (token){
                 case "swap":
-                    Collections.swap(numbers, Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    int index1 = Integer.parseInt(commands[1]);
+                    int index2 = Integer.parseInt(commands[2]);
+                    if (index1 >= 0 && index1 < numbers.length && index2 >= 0 && index2 < numbers.length){
+                        int temp = numbers[index1];
+                        numbers[index1] = numbers[index2];
+                        numbers[index2] = temp;
+                    }
                     break;
                 case "multiply":
-                    int result;
-                    int firstEl = numbers.get(Integer.parseInt(tokens[1]));
-                    int secondEl = numbers.get(Integer.parseInt(tokens[2]));
-                    result = firstEl * secondEl;
-                    numbers.set(numbers.indexOf(firstEl), result);
+                    index1 = Integer.parseInt(commands[1]);
+                    index2 = Integer.parseInt(commands[2]);
+                    if (index1 >= 0 && index1 < numbers.length && index2 >= 0 && index2 < numbers.length){
+                        int temp = numbers[index1] * numbers[index2];
+                         numbers[index1] = temp;
+                    }
                     break;
                 case "decrease":
-                    for (int i = 0; i < numbers.size(); i++) {
-                        numbers.set(i, numbers.get(i) - 1);
+                    for (int i = 0; i <numbers.length ; i++) {
+                        int temp = numbers[i] - 1;
+                        numbers[i] = temp;
                     }
                     break;
             }
-
-            commands = scanner.nextLine();
+ 
+            input = scan.nextLine();
         }
-        boolean isFirst = true;
-        for (int n : numbers) {
-            if (isFirst) {
-                System.out.print(n);
-                isFirst = false;
-            } else {
-                System.out.print(", "+n);
+        for (int i = 0; i <numbers.length ; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1){
+                System.out.print(", ");
             }
         }
     }
