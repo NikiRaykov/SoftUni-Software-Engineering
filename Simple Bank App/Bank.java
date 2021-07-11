abf203b5af680c743abc3acec42c78ec042257e6
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-07-11 09:28:32 +0200

    Delete Simple Bank App directory

--- Simple Bank App/Bank.java
+++
@@ -1,9 +0,0 @@
-public class Bank {
-    public static void main(String[] args) {
-
-        Account niki = new Account("Niki Raykov", "BG199600");
-
-        Account yana = new Account("Yana Borisova", "Y200015");
-        yana.showMenu();
-    }
-}
