2f19722a1a2b90c6f47b03577ab5a42013e9475a
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-07-12 21:35:13 +0200

    Add files via upload

---
+++ 2. Programming Fundamentals with Java/Maps, Lambda and Stream API/WordFilter.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = Arrays.stream(scanner.nextLine().split(" ")).filter(e -> e.length() % 2 == 0).toArray(String[]::new);
+
+        for (String word: input){
+            System.out.println(word);
+        }
+    }
+}
