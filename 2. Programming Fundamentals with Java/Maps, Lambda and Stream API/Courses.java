81460b525620e8d40865c767a92e464bc0f73d1f
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-07-14 17:16:38 +0200

    Add files via upload

---
+++ 2. Programming Fundamentals with Java/Maps, Lambda and Stream API/Courses.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Courses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<String, List<String>> courses = new LinkedHashMap<>();
+
+        while (!input.equals("end")){
+            String courseName = input.split(" : ")[0];
+            String studentName = input.split(" : ")[1];
+
+
+            if (!courses.containsKey(courseName)){
+                courses.put(courseName, new ArrayList<>());
+            }
+                courses.get(courseName).add(studentName);
+
+
+            input = scanner.nextLine();
+        }
+        courses.entrySet().stream()
+                .sorted((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
+                .forEach(e -> {
+                    System.out.println(e.getKey() + ": " + e.getValue().size());
+                    e.getValue().stream()
+                            .sorted(String::compareTo)
+                            .forEach(student -> System.out.println("-- " + student));
+                });
+    }
+}
