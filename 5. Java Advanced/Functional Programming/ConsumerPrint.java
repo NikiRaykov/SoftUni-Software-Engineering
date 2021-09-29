e37274c88a744e4df04c5ae610f09912c56907bb
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-09-29 20:53:15 +0200

    Add files via upload

---
+++ 5. Java Advanced/Functional Programming/ConsumerPrint.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> people = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        Consumer<List<String>> printPeople = strings -> {
+            strings.forEach(System.out::println);
+        };
+
+        printPeople.accept(people);
+    }
+}
