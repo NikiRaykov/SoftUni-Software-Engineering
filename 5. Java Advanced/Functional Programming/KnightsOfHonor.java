ba3aee97adc0486400cfed747418a456f66c087c
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-09-30 23:34:03 +0200

    Add files via upload

---
+++ 5. Java Advanced/Functional Programming/KnightsOfHonor.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] names = input.split("\\s+");
+        Consumer<String> printWithSir = name -> System.out.println("Sir " + name);
+        Arrays.stream(names).forEach(printWithSir);
+    }
+}
