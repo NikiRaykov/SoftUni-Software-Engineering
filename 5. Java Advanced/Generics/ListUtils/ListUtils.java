ef3fc26456b72b5010c3114082afd15e1ece31f3
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-10-20 19:12:03 +0200

    Add files via upload

---
+++ 5. Java Advanced/Generics/ListUtils/ListUtils.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.List;
+
+public class ListUtils{
+
+
+    public static <T extends Comparable<T>> T getMin(List<T> list){
+        if (list.isEmpty()){
+            throw new IllegalArgumentException();
+        }else {
+            return Collections.min(list);
+        }
+    }
+
+    public static <T extends Comparable<T>> T getMax(List<T> list){
+        if (list.isEmpty()){
+            throw new IllegalArgumentException();
+        }else {
+            return Collections.max(list);
+        }
+    }
+}
