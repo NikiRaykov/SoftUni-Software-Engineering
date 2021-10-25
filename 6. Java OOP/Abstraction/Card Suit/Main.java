744d3808f84491ccaf1ea154087ada17140bae75
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-10-25 18:42:28 +0200

    Update and rename deleteMe.java to Main.java

---
+++ 6. Java OOP/Abstraction/Card Suit/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        System.out.println(input + ":");
+        for (Cards cards: Cards.values()){
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cards.ordinal(), cards.name());
+        }
+    }
+}
