d3566a7544190d3b5319b707a54f1b617ea1a824
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-10-28 12:45:13 +0200

    Add files via upload

---
+++ 6. Java OOP/Abstraction/Point in Rectangle/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    private final Point pointA;
+    private final Point pointC;
+
+    public Rectangle(Point pointA, Point pointC) {
+        this.pointA = pointA;
+        this.pointC = pointC;
+    }
+
+    public boolean isInside(Point p) {
+        return p.isGreatThanOrEqual(pointA) && p.isLeesThanOrEqual(pointC);
+    }
+}
