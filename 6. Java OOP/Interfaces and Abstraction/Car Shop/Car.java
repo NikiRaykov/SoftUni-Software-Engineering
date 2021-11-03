9169b1edbdc7be8bfba149cbbe7e66aa535d4bf8
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-11-03 17:13:43 +0100

    Add files via upload

---
+++ 6. Java OOP/Interfaces and Abstraction/Car Shop/Car.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    int TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    int getHorsePower();
+
+    String countryProduced();
+}
