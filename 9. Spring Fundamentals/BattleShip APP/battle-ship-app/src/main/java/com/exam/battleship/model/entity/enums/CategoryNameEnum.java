b9a36d30e44b5cddc56a8f5d93f49fcd97b36853
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2022-06-16 20:39:22 +0200

    Delete 9. Spring Fundamentals/BattleShip APP/battle-ship-app directory

--- 9. Spring Fundamentals/BattleShip APP/battle-ship-app/src/main/java/com/exam/battleship/model/entity/enums/CategoryNameEnum.java
+++
@@ -1,6 +0,0 @@
-package com.exam.battleship.model.entity.enums;
-
-public enum CategoryNameEnum {
-
-    BATTLE, CARGO, PATROL
-}
--- 9. Spring Fundamentals/BattleShip APP/battle-ship-app/src/main/java/com/exam/battleship/service/ShipService.java
+++
@@ -1,7 +0,0 @@
-package com.exam.battleship.service;
-
-import com.exam.battleship.model.entity.service.ShipServiceModel;
-
-public interface ShipService {
-    void add(ShipServiceModel shipServiceModel);
-}
