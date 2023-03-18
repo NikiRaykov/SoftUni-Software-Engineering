9a4c31ebe645dc0002614f79388e636ffd27defb
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-03-18 21:10:30 +0100

    Add files via upload

---
+++ 9. JS Front-End/Functions and Statements/FormatGrade.js
@@ -0,0 +1,15 @@
+function solve(grade) {
+    if (grade < 3.00) {
+        console.log("Fail (2)");
+    } else if (grade >= 3.00 && grade < 3.50) {
+        console.log("Poor" + " " + (grade.toFixed(2)));
+    } else if (grade >= 3.50 && grade < 4.50) {
+        console.log("Good" + " " + (grade.toFixed(2)));
+    } else if (grade >= 4.50 && grade < 5.50) {
+        console.log("Very Good" + " " + (grade.toFixed(2)));
+    } else if (grade >= 5.50) {
+        console.log("Excellent" + " " + (grade.toFixed(2)));
+    }
+}
+
+solve(4.60);
