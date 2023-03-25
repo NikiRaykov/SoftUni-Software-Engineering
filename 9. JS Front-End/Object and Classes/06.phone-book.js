3b5e885483b56053b26b09b9725054bede85a717
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-03-25 21:15:15 +0100

    Add files via upload

---
+++ 9. JS Front-End/Object and Classes/06.phone-book.js
@@ -0,0 +1,19 @@
+function phoneBookParses(input) {
+    let phoneBook = {};
+
+    for (const line of input) {
+        let [ name, phoneNumber ] = line.split(' ');
+        phoneBook[name] = phoneNumber;
+    }
+
+    for (const key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`);
+    }
+}
+
+phoneBookParses(
+    ['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']
+);
