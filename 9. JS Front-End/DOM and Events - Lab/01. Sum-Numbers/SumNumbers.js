e399a2fc4cdc196a07e21d0988fb2d88ccc808e2
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-03-30 16:59:02 +0200

    Add files via upload

---
+++ 9. JS Front-End/DOM and Events - Lab/01. Sum-Numbers/SumNumbers.js
@@ -0,0 +1,11 @@
+function calc() {
+    const firstInput = document.getElementById('num1');
+    const secondInput = document.getElementById('num2');
+    const sumInput = document.getElementById('sum');
+
+    let firstNum = Number(firstInput.value);
+    let secondNum = Number(secondInput.value);
+
+    let sum = firstNum + secondNum;
+    sumInput.value = sum;
+}
