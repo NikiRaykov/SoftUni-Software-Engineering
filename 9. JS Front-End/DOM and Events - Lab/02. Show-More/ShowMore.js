44482c65f9a12cdc4398819999d37b486f9ed68a
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-03-31 11:56:42 +0200

    Add files via upload

---
+++ 9. JS Front-End/DOM and Events - Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const anchor = document.getElementById('more');
+    const span = document.getElementById('text');
+
+    span.style.display = 'inline';
+    anchor.style.display = 'none';
+}
