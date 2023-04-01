ed27495eb21d824821c84cf6df50bec76e6b4adf
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-01 12:35:44 +0200

    Add files via upload

---
+++ 9. JS Front-End/DOM and Events - Lab/05. Delete-From-Table/app.js
@@ -0,0 +1,17 @@
+function deleteByEmail() {
+
+    const result = document.getElementById('result');
+    const input = document.querySelector('input[name="email"]') // we do not have an ID
+    
+    const evenTds = Array.from(document.querySelectorAll('td:nth-child(even)')); // we take only email values in <td>
+    
+    let emailValue = input.value;
+    let foundElement = evenTds.find((td) => td.textContent === emailValue);
+
+    if (foundElement) {
+        foundElement.parentElement.remove();
+        result.textContent = 'Deleted';
+    } else {
+        result.textContent = 'Not found.';
+    }
+}
