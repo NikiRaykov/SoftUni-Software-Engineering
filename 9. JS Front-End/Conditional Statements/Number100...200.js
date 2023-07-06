4ff99a2549f17273c65481d9528255f9c6199fae
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-07-06 19:47:51 +0200

    Add files via upload

---
+++ 9. JS Front-End/Conditional Statements/Number100...200.js
@@ -0,0 +1,15 @@
+function numberBetween(input) {
+
+    let number = Number(input[0]);
+    
+    if (number < 100) {
+        console.log("Less than 100")
+    } else if (number >= 100 && number <= 200) {
+        console.log("Between 100 and 200")
+    } else {
+        console.log("Greater than 200")
+    }
+
+}
+
+numberBetween(["95"])
